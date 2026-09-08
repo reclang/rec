2fc4d4a2ee79868fcefa784dfef4c37f8c11fced
Vladimir Fedorov <fedorovvs@gmail.com>
2026-09-08 16:22:30 +0200

    Normalize source file paths

--- src/args.d
+++ src/args.d
@@ -59,6 +59,8 @@ Arguments processArguments(string[] args) {
 	string currentPath = ".".normalizePath;
 	arguments.path = arguments.path.length == 0 ? currentPath : arguments.path.normalizePath;
 	arguments.include = arguments.include.length == 0 ? currentPath : arguments.include.normalizePath;
+	// filenames are relative to the source path; absolute ones are kept as is
+	foreach (ref filename; arguments.filenames) filename = buildNormalizedPath(arguments.path, filename);
 	if (arguments.name.length == 0) {
 		if (arguments.outputFile.length != 0) {
 			arguments.name = arguments.outputFile.withoutExtension;
