1fb84865e07ce79f656a02bc18f4fbac8534f6fb
Vladimir Fedorov <fedorovvs@gmail.com>
2026-09-15 16:49:12 +0200

    Tokenizer optimization

--- src/tokenizer.d
+++ src/tokenizer.d
@@ -3,70 +3,68 @@ module tokenizer;
 import std.uni;
 import pp;
 
+enum CharKind: ubyte { other, digit, white, punct, quote }
+CharKind[256] charKind = () {
+    CharKind[256] ck; // defaults to other
+    foreach(c; "0123456789")  ck[c] = CharKind.digit;
+    foreach(c; " \t\n\r\v\f") ck[c] = CharKind.white;
+    foreach(c; "{}(),")       ck[c] = CharKind.punct;
+    foreach(c; "\"")          ck[c] = CharKind.quote;
+    return ck;
+}();
+
 enum TokenKind {
-	identifier, type, keyword, constant, punctuator, text
+    identifier, type, keyword, constant, punctuator, text
 }
 
-bool[string] types = [ "void": true ];
-bool[string] punctuators = [ "{": true, "(": true, "}": true, ")": true,  ",": true  ];
-
 struct Token {
-	string path;
-	string filename;
-	uint line;
-	uint pos;
-	string text;
-	TokenKind kind;
+    string path;
+    string filename;
+    uint line;
+    uint pos;
+    string text;
+    TokenKind kind;
 }
 
 TokenKind tokenKind(string s) {
-	if (s in types) return TokenKind.type;
-	if (isNumber(s[0])) return TokenKind.constant;
-	if (s in punctuators) return TokenKind.punctuator;
-	return TokenKind.identifier;
+    switch(s) {
+        case "void": return TokenKind.type;
+        default: return charKind[s[0]] == CharKind.digit ? TokenKind.constant : TokenKind.identifier;
+    }
+}
+
+private void tokenizeLine(ref Token[] tokens, string path, string filename, uint line, string s) {
+    uint i = 0;
+    while(i < s.length) {
+        final switch (charKind[s[i]]) {
+            case CharKind.white: i++; break;
+            case CharKind.punct:
+                tokens ~= Token(path, filename, line, i, s[i .. i+1], TokenKind.punctuator);
+                i++;
+                break;
+            case CharKind.quote:
+                uint start = i++;
+                while (i < s.length && s[i] != '"') i++;
+                tokens ~= Token(path, filename, line, start, s[start + 1 .. i], TokenKind.text);
+                i++;
+                break;
+            case CharKind.other, CharKind.digit:
+                uint start = i++;
+                while (i < s.length && charKind[s[i]] != CharKind.white && charKind[s[i]] != CharKind.punct) i++;
+                tokens ~= Token(path, filename, line, start, s[start .. i], tokenKind(s[start .. i]));
+                break;
+        }
+    }
 }
 
 Token[] tokenize(string path, string filename, uint line, string s) {
-	Token[] tokens;
-	uint start;
-	bool collecting;
-	for (uint i = 0; i < s.length; i++) {
-		char c = s[i];
-		if (collecting) {
-			if (isWhite(c)) {
-				collecting = false;
-				tokens ~= Token(path, filename, line, start, s[start..i], tokenKind(s[start..i]));
-				continue;
-			}
-			switch (c) {
-				case '{', '(', '}', ')', ',':
-					tokens ~= Token(path, filename, line, start, s[start..i], tokenKind(s[start..i]));
-					tokens ~= Token(path, filename, line, i, s[i..i+1], TokenKind.punctuator);
-					start = i+1;
-					collecting = false;
-					break;
-				default: continue;
-			}
-		} else {
-			if (isWhite(c)) continue;
-			if (c == '"') {
-				start = i;
-				while (++i < s.length && s[i] != '"') { }
-				tokens ~= Token(path, filename, line, start, s[start+1..i], TokenKind.text);
-				continue;
-			}
-			collecting = true;
-			start = i;
-		}
-	}
-	if (collecting) tokens ~= Token(path, filename, line, start, s[start..$], tokenKind(s[start..$]));
-	return tokens;
+   Token[] tokens;
+    tokenizeLine(tokens, path, filename, line, s);
+    return tokens;
 }
 
 Token[] tokenize(SourceLine[] lines) {
-	Token[] tokens;
-	foreach(line; lines) {
-		tokens ~= tokenize(line.path, line.filename, line.num, line.text);
-	}
-	return tokens;
+    Token[] tokens;
+    foreach(ref line; lines) tokenizeLine(tokens, line.path, line.filename, line.num, line.text);
+    return tokens;
 }
