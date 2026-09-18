ca03b4e0911711251e86cafe27f0fc5828b95a32
Vladimir Fedorov <fedorovvs@gmail.com>
2026-09-18 15:11:28 -0400

    Output errors to stderr

--- src/reclang.d
+++ src/reclang.d
@@ -30,7 +30,7 @@ void showHelp() {
 int main(string[] args) {
 	Arguments arguments = processArguments(args);
 	if (arguments.error.length != 0) {
-		writeln(arguments.error);
+		stderr.writeln(arguments.error);
 		return 1;
 	}
 	if (arguments.showVersion) {
